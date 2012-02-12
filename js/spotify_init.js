7c745e95bb0b35a59d332b57b63fa38fa3ca71de
Chris Roane <croane@thebrickfactory.com>
2012-02-12 16:01:09 -0700

    Issue #[1425392] by klausi: Re-worked the JS and applied some major changes
    - Surrounded some output text that I missed with the t() function.
    - Took out the help text from the main config page and included it in hook_help(). This is in the .module file that was in the previous commit.
    - Took out some basic JavaScript and put it into a separate JS file.
    - Took out using the filter_xss() function where it was needed.
    - Simplified a few if statements.
    - I re-wrote the whole splash display mechanism. All of the JavaScript is now in a separate file and we send drupal settings variables to use in the JS. This makes the JS much easier to maintain and debug. This required a lot of re-working in the splashify_init() function.

---
+++ js/spotify_init.js
@@ -0,0 +1,56 @@
+(function ($) {
+
+  Drupal.behaviors.splashify = {
+    attach: function (context, settings){
+
+      var splash = $.jStorage.get("splash");
+      var nowtimeSeconds = settings.splashify.js_nowtime;
+      var splashalways = settings.splashify.js_splash_always;
+      if(!splash || splash < nowtimeSeconds || splashalways=='1'){
+        // Set when the splash variable should expire.
+        $.jStorage.set("splash", settings.splashify.js_expiretime);
+
+        // If we are displaying each splash page in sequence, we need to
+        // determine the next url.
+        if(settings.splashify.js_mode_settings.urls){
+          var new_url_index = 0;
+          var last_url = $.jStorage.get('splashlasturl', '');
+          var what_urls = settings.splashify.js_mode_settings.urls;
+          var last_url_index = what_urls.indexOf(last_url);
+          if(last_url_index > -1 && last_url_index+1 < settings.splashify.js_mode_settings.total_urls){
+            new_url_index = last_url_index + 1;
+          }
+          var url = what_urls[new_url_index];
+          $.jStorage.set('splashlasturl', url);
+
+          // We have the correct url, now set the main url value.
+          settings.splashify.js_mode_settings.url = url;
+        }
+
+        // Do the splash action.
+        if(settings.splashify.js_mode == 'colorbox'){
+          // Open a ColorBox
+          $(window).load(function(){
+            $.colorbox({
+              transition:'elastic',
+              iframe:'true',
+              href:settings.splashify.js_mode_settings.url,
+              width:settings.splashify.js_mode_settings.size_width,
+              height:settings.splashify.js_mode_settings.size_height
+            });
+          });
+        } else if(settings.splashify.js_mode == 'redirect'){
+          // Redirect
+          window.location.replace(settings.splashify.js_mode_settings.url);
+        } else if(settings.splashify.js_mode == 'window'){
+          // Open a popup window
+          $(window).load(function(){
+            window.open(settings.splashify.js_mode_settings.url, 'splash', settings.splashify.js_mode_settings.size);
+          });
+        }
+      }
+
+    }
+  };
+
+})(jQuery);
