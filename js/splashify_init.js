0545953bcf014bc0af62291b383fba13be7dd925
Chris Roane <croane@thebrickfactory.com>
2012-11-07 09:28:03 -0700

    Fixed random splash page with Page Caching On and more Testing
    I discovered that the new JS was incomplete because it did not
    handle the option for displaying a random splash page (like it
    was doing with the sequential option). I added this new JS code
    and also moved the JS around a little bit. I also cleaned up
    some of the PHP code.
    
    Fixed a bug in the JS code with the sequence option in IE7 and
    IE8. indexOf() doesn't exist in these browsers, so replaced
    with the jQuery equivalent.
    
    I also did more testing in IE7, IE8 and IE9 to make sure the
    new JS code worked in all cases.

--- js/splashify_init.js
+++ js/splashify_init.js
@@ -12,47 +12,55 @@
         }
         var splash = $.jStorage.get("splash");
         var expireAfter = settings.splashify.js_expire_after;
+        var splashalways = settings.splashify.js_splash_always;
         var now = new Date();
         var nowtimeSeconds = now.getTime() / 1000;
 
-        var splashalways = settings.splashify.js_splash_always;
+        // Display the splash page?
         if(show == 1 && (!splash || splash < nowtimeSeconds || splashalways=='1')){
           // Set when the splash variable should expire.
           $.jStorage.set("splash", nowtimeSeconds + expireAfter);
 
-          // If we are displaying each splash page in sequence, we need to
-          // determine the next url.
-          if(settings.splashify.js_mode_settings.urls){
+          var url = '';
+          var last_url = $.jStorage.get('splashlasturl', '');
+          var what_urls = settings.splashify.js_mode_settings.urls;
+
+          // Determine the url we are working with, which is based on the mode.
+          if(settings.splashify.js_mode_settings.system_splash != ''){
+            // Display the system splash page.
+            url = settings.splashify.js_mode_settings.system_splash;
+          } else if(settings.splashify.js_mode_settings.mode == 'sequence'){
+            // Display the splash pages in sequence.
             var new_url_index = 0;
-            var last_url = $.jStorage.get('splashlasturl', '');
-            var what_urls = settings.splashify.js_mode_settings.urls;
-            var last_url_index = what_urls.indexOf(last_url);
+            var last_url_index = jQuery.inArray(last_url, what_urls);
             if(last_url_index > -1 && last_url_index+1 < settings.splashify.js_mode_settings.total_urls){
               new_url_index = last_url_index + 1;
             }
-            var url = what_urls[new_url_index];
-            $.jStorage.set('splashlasturl', url);
-
-            // We have the correct url, now set the main url value.
-            settings.splashify.js_mode_settings.url = url;
+            url = what_urls[new_url_index];
+          } else if(settings.splashify.js_mode_settings.mode == 'random'){
+            // Display a random splash page.
+            var new_url_index = Math.floor(Math.random() * settings.splashify.js_mode_settings.total_urls);
+            url = what_urls[new_url_index];
           }
 
-          // Do the splash action.
+          $.jStorage.set('splashlasturl', url);
+
+          // Display the splash page.
           if(settings.splashify.js_mode == 'colorbox'){
-            // Open a ColorBox
+            // Open a ColorBox.
             $.colorbox({
               transition:'elastic',
               iframe:settings.splashify.js_mode_settings.iframe_option,
-              href:settings.splashify.js_mode_settings.url,
+              href:url,
               width:settings.splashify.js_mode_settings.size_width,
               height:settings.splashify.js_mode_settings.size_height
             });
           } else if(settings.splashify.js_mode == 'redirect'){
-            // Redirect
-            window.location.replace(settings.splashify.js_mode_settings.url);
+            // Redirect.
+            window.location.replace(url);
           } else if(settings.splashify.js_mode == 'window'){
-            // Open a popup window
-            window.open(settings.splashify.js_mode_settings.url, 'splash', settings.splashify.js_mode_settings.size);
+            // Open a popup window.
+            window.open(url, 'splash', settings.splashify.js_mode_settings.size);
           }
         }
       });
